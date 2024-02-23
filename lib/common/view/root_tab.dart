db78ddd64ab212866171a6036a40ceb873151a66
mark0620 <mark0620@naver.com>
2024-02-23 21:43:31 +0900

    RestaurantCard 작업하기

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,5 +1,6 @@
 import 'package:advancedflutter/common/const/colors.dart';
 import 'package:advancedflutter/common/layout/default_layout.dart';
+import 'package:advancedflutter/restaurant/view/restaurant_screen.dart';
 import 'package:flutter/material.dart';
 
 class RootTab extends StatefulWidget {
@@ -11,17 +12,45 @@ class RootTab extends StatefulWidget {
   State<RootTab> createState() => _RootTabState();
 }
 
-class _RootTabState extends State<RootTab> {
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
   int index = 0;
+  late TabController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = TabController(length: 4, vsync: this);
+    controller.addListener(tabListener);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  void tabListener(){
+    setState(() {
+      index = controller.index;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '코팩 딜리버리',
-      child: Center(
-        child: Text(
-          'Root Tab',
-        ),
+      child: TabBarView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: this.controller,
+        children: [
+          RestaurantScreen(),
+          Center(child: Container(child: Text('음식'),),),
+          Center(child: Container(child: Text('주문'),),),
+          Center(child: Container(child: Text('프로필'),),),
+        ],
       ),
       bottomNaviagationBar: BottomNavigationBar(
         selectedItemColor: PRIMARY_COLOR,
@@ -29,10 +58,8 @@ class _RootTabState extends State<RootTab> {
         selectedFontSize: 10,
         unselectedFontSize: 10,
         type: BottomNavigationBarType.fixed,
-        onTap: (int index){
-          setState(() {
-            this.index = index;
-          });
+        onTap: (int index) {
+          controller.animateTo(index);
         },
         currentIndex: index,
         items: [
