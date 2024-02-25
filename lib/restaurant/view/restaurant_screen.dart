f2265c7edf8a3c7bc373638cd3f71194b153faf6
mark0620 <mark0620@naver.com>
2024-02-26 01:01:24 +0900

    완성된 Pagination로직 실행해보기

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -7,26 +7,60 @@ import 'package:advancedflutter/restaurant/view/restaurant_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller.addListener(scrollListener);
+  }
+  void scrollListener(){
+
+    if(controller.offset > controller.position.maxScrollExtent - 300 ){
+      ref.read(restaurantProvider.notifier).paginate(
+          fetchMore: true,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
+    //완전 처음 로딩일 때
     if(data is CursorPaginationLoading){
       return Center(
         child: CircularProgressIndicator(),
       );
     }
 
+    if (data is CursorPaginationError){
+      return Center(
+        child: Text(data.message),
+      );
+    }
+
+    //CursorPatingaion
+    //CursorPatingaionFetchingMore
+    //CursorPatingaionRefetching
+
     final cp = data as CursorPagination;
 
     return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: ListView.separated(
+          controller: controller,
           itemCount: cp.data.length,
           itemBuilder: (_, index) {
             final pItem = cp.data[index];
