970d1cdda2d7fd5000e3de3048736d9c28004233
mark0620 <mark0620@naver.com>
2024-03-03 16:19:05 +0900

    get Me API 작업하기, 보완

--- lib/user/model/user_model.dart
+++ lib/user/model/user_model.dart
@@ -3,8 +3,22 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'user_model.g.dart';
 
+abstract class UserModelBase {}
+
+class UserModelError extends UserModelBase {
+  final String message;
+
+  UserModelError({
+    required this.message,
+  });
+}
+
+class UserModelLoading extends UserModelBase {
+
+}
+
 @JsonSerializable()
-class UserModel{
+class UserModel extends UserModelBase {
   final String id;
   final String username;
   @JsonKey(
@@ -16,7 +30,8 @@ class UserModel{
     required this.id,
     required this.username,
     required this.imageUrl,
-});
-  factory UserModel.fromJson(Map<String, dynamic> json)
-  => _$UserModelFromJson(json);
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
 }
