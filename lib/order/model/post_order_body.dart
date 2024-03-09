01acb54ba8b8e9e06b0aceac5d02c04367d3cad0
mark0620 <mark0620@naver.com>
2024-03-09 18:23:35 +0900

    OrderProvider 작업하기

--- lib/order/model/post_order_body.dart
+++ lib/order/model/post_order_body.dart
@@ -4,9 +4,9 @@ import 'package:json_annotation/json_annotation.dart';
 part 'post_order_body.g.dart';
 
 @JsonSerializable()
-class PostOrderBody{
+class PostOrderBody {
   final String id;
-  final List<BasketItemModel> products;
+  final List<PostOrderBodyProduct> products;
   final int totalPrice;
   final String createAt;
 
@@ -15,10 +15,26 @@ class PostOrderBody{
     required this.products,
     required this.totalPrice,
     required this.createAt,
-});
-  
-  factory PostOrderBody.fromJson(Map<String, dynamic> json)
-  => _$PostOrderBodyFromJson(json);
-  
+  });
+
+  factory PostOrderBody.fromJson(Map<String, dynamic> json) =>
+      _$PostOrderBodyFromJson(json);
+
   Map<String, dynamic> toJson() => _$PostOrderBodyToJson(this);
+}
+
+@JsonSerializable()
+class PostOrderBodyProduct {
+  final String productId;
+  final int count;
+
+  PostOrderBodyProduct({
+    required this.productId,
+    required this.count,
+  });
+
+  factory PostOrderBodyProduct.fromJson(Map<String, dynamic> json)
+  => _$PostOrderBodyProductFromJson(json);
+
+  Map<String,dynamic> toJson() => _$PostOrderBodyProductToJson(this);
 }
